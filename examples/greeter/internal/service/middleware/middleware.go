01d4f88952e911001bed385c4add50be041a63be
Lukas <lukas.jarosch@mail.com>
2019-06-25 18:58:21 +0200

    remove examples from main repo
    Signed-off-by: Lukas <lukas.jarosch@mail.com>

--- examples/greeter/internal/service/middleware/middleware.go
+++
@@ -1,7 +0,0 @@
-package middleware
-
-import (
-	"yyy/internal/service"
-)
-
-type Middleware func(service service.Yyy) service.Yyy
--- examples/user/internal/service/middleware/middleware.go
+++
@@ -1,7 +0,0 @@
-package middleware
-
-import (
-	"github.com/lukasjarosch/godin/examples/user/internal/service"
-)
-
-type Middleware func(service service.User) service.User
